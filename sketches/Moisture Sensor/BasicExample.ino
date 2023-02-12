fd9ecb14816b16bee093e9f0ecfb139b83c99bcc
Marlon Böhland <kamephis@gmail.com>
2023-02-12 21:47:52 +0100

    Update BasicExample.ino
    Print out the values on the lcd

--- sketches/Moisture Sensor/BasicExample.ino
+++ sketches/Moisture Sensor/BasicExample.ino
@@ -1,39 +1,35 @@
-/*
-  This example uses a moisture sensor and prints out a percentage level based on the
-  soilMoistLeveLow and soilMoistLevelHigh. This highly depends on your plants.
-
-  Pin Settings
-  
-  Arduino UNO: + = 5V, - = GND, A = A0
-  ESP32: + = 3.3V, - GND, A = A0 (36) / VP
-  ESP8266: + 3,3V, - GND, A = A0
-  Arduino Mega: + 5V, - GND, A = A0
-*/
+#include <LiquidCrystal.h>
 
 const int soilMoistPin = A0;
-const int soilMoistLevelLow = 200;
-const int soilMoistLevelHigh = 800;
+const int soilMoistLevelLow = 844;
+const int soilMoistLevelHigh = 377;
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 void setup()
 {
   Serial.begin(115200);
   pinMode(soilMoistPin, INPUT);
+  lcd.begin(16, 2);
+  lcd.print("Soil Moisture");
 }
 
 void loop()
 {
   int soilMoist = analogRead(soilMoistPin);
-  Serial.print("Analog Value: ");
-  Serial.print(soilMoist);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Analog Value: ");
+  lcd.print(soilMoist);
 
   if (soilMoist >= soilMoistLevelLow && soilMoist <= soilMoistLevelHigh) {
     int percentSoilMoist = map(soilMoist, soilMoistLevelHigh, soilMoistLevelLow, 100, 0);
-    Serial.print("\t");
-    Serial.print(percentSoilMoist);
-    Serial.println(" %");
+    lcd.print("\t");
+    lcd.print(percentSoilMoist);
+    lcd.print(" %");
   } else {
-    Serial.println("\tNo moisture detected.");
+    lcd.println("\tNo moisture");
   }
-  
+
   delay(2000);
 }
